dab3ba1171d1f14f1cfb2631c3e6b66502e2c6af
Joao <joaovppo@gmail.com>
2025-10-23 21:04:57 -0300

    adicionadas classes de security

---
+++ src/main/java/com/joliveira/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.joliveira.usuario.infrastructure.repository;
+
+import com.oliveirajoao.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/joliveira/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.joliveira.usuario.infrastructure.repository;
+
+import com.oliveirajoao.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
