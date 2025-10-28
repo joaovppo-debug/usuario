2cf2f9ad5c053267fa908bd55b52587bb1db6fc0
Joao <joaovppo@gmail.com>
2025-10-28 09:55:26 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/joliveira/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/joliveira/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.joliveira.usuario.infrastructure.repository;
 
-import com.oliveirajoao.aprendendospring.infrastructure.entity.Endereco;
+
+import com.joliveira.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
